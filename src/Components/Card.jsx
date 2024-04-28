b22a908ce8d698513113dda7365a817aae271f72
Zahid Hasan <imzahidhasan.mail@gmail.com>
2024-04-28 15:38:07 +0600

    added sort button for ascending and descending sort

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom'
 
 const Card = ({ spot }) => {
     console.log(spot);
-    const { description, location, photoURL, country_name, tourist_spot_name, _id }=spot
+    const { description, totalVisitorsPerYear, travel_time, seasonality,photoURL, average_cost, tourist_spot_name, _id }=spot
   return (
       <div className='mx-auto'>
           <div className="bg-gray-200 p-4 rounded-lg shadow-md">
               <img className="w-full min-h-52 max-h-52 mb-4" src={photoURL}  />
               <div className="">
                   <h2 className="text-lg font-bold mb-2">{tourist_spot_name}</h2>
-                  <p  className='text-gray-700 mb-2' >{ `${location}, ${country_name}`}</p>
+                  <p  className='text-gray-700 mb-2' >{ `Average cost: ${average_cost}$`}</p>
+                  <p className='text-gray-700 mb-2' >{`total Visitors Per Year: ${totalVisitorsPerYear}$`}</p>
+                  <p  className='text-gray-700 mb-2' >{ `Travel time: ${travel_time}$`}</p>
+                  <p  className='text-gray-700 mb-2' >{ `Seasonality : ${seasonality}$`}</p>
                   <p className="text-gray-700 hyphens-auto">{`${description.slice(0,120)}....` }
                   </p>
                   <Link to={`/details/${_id}`}>
--- src/Pages/AllTouristSpot.jsx
+++ src/Pages/AllTouristSpot.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../Components/Card'
 import { useLoaderData } from 'react-router-dom'
 import Heading from '../Components/Heading'
 
 const AllTouristSpot = () => {
-  const spot_data = useLoaderData()
-  console.log(spot_data);
+  const data = useLoaderData()
+  const [spot_data, setSpotData] = useState(data)
+  const handleAscendingSort = () => {
+    fetch(``)
+      .then(res => res.json())
+      .then(result => setSpotData(result))
+  }
+  const handleDescendingSort = () => {
+    fetch(``)
+      .then(res => res.json())
+      .then(result => setSpotData(result))
+  }
+
+
+
   return (
     <>
-      <Heading text={'Best Places to Visit'} para={ `
+      <Heading text={'Best Places to Visit'} para={`
 From the towering peaks of the Swiss Alps to the sun-kissed beaches of Bali, the world's best places to visit promise awe-inspiring beauty and unforgettable experiences. Whether it's exploring the bustling streets of New York City or marveling at the grandeur of the Taj Mahal, there's something to captivate every traveler's heart and soul.`} />
-    <div className=' container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3'>
-      {
-        spot_data.map((spot) => <Card key={spot._id} spot={spot}></Card>)
-      }
+
+      <label className="mb-2 ">Sort by</label>
+      <div className="relative">
+        <select className="inline-block appearance-none  text-center bg-white border border-gray-300 hover:border-gray-500 px-4 py-4  rounded shadow leading-tight focus:dark:ring-violet-600 dark:border-gray-300"
+        >
+          <option disabled >select</option>
+          <option onClick={handleAscendingSort} >Ascending</option>
+          <option onClick={handleDescendingSort} >Descending</option>
+        </select>
+      </div>
+      <div className=' container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3'>
+
+        {
+          spot_data.map((spot) => <Card key={spot._id} spot={spot}></Card>)
+        }
       </div>
     </>
   )
